a7c1d96d7d5a4e6933292e099221b4260827c81f
TMM/416632 <vinoth.kumar.murugan@toyota.com>
2019-05-24 06:02:52 -0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'add',component:AddTaskComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
