bc9db4049c828862772f17cc8af039db8e1bbcc5
Vinoth <Vinoth@LAPTOP-UFQCNOCD>
2019-05-25 00:39:35 +0530

    commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+
+const routes: Routes = [{
+  path:'add',component:AddTaskComponent},
+  {path:'view',component:ViewTaskComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
